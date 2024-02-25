2d4f4f79983112645cc003a7fc5eb01dfa9f01a1
Nina Andric <andric.nina2000@gmail.com>
2024-02-25 16:48:49 +0100

    zamenjena verzija php-a i dodate migracije

---
+++ pinterestAPI/database/migrations/2024_02_25_151813_create_pins_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pins', function (Blueprint $table) {
+            $table->id();
+            $table->string('pin_title');
+            $table->text('pin_description');
+            $table->string('image');
+            $table->unsignedBigInteger('board_id');
+            $table->foreign('board_id')->references('id')->on('boards');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pins');
+    }
+};
